6bb98cb25e54743a5ad9df4b151e936bad6bb9df
Riya4530 <riyahpatel837@gmail.com>
2026-06-11 18:13:48 +0530

    fixed port for render

--- Backend/server.js
+++ Backend/server.js
@@ -2419,14 +2419,10 @@ app.get(
    SERVER START
 ========================= */
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-
-  console.log(
-    `Server running on port ${PORT}`
-  );
-
+  console.log(`Server running on port ${PORT}`);
 });
 
 // Setup a dedicated listener client for Postgres LISTEN/NOTIFY
