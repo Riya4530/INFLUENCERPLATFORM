74a277ee651a7211c615bef4d8977fa76c0e4493
Riya4530 <riyahpatel837@gmail.com>
2026-06-12 19:02:34 +0530

    fixed

--- Backend/server.js
+++ Backend/server.js
@@ -19,8 +19,7 @@ app.use(cors({
   ],
    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true
-}));
-app.options("*", cors());
+}));app.options(/.*/, cors());
 app.use(express.json());
 
 // Simple in-memory SSE clients map
