3db13c983319991799e1348f37a3c1b156088fdb
Riya4530 <riyahpatel837@gmail.com>
2026-06-14 00:56:51 +0530

    contact msgs

--- frontend/src/app/admin/message/page.tsx
+++ frontend/src/app/admin/message/page.tsx
@@ -10,7 +10,7 @@ export default function ContactMessagesPage() {
     const fetchMessages = async () => {
       try {
         const res = await fetch(
-          `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/contact`
+          `${process.env.NEXT_PUBLIC_API_URL}/api/contact`
         );
         const data = await res.json();
 
